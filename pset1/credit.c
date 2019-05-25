a2c352aff4b3ae3f443fb61635590e63120520b6
Tom-McDonald <tommcdonald955@gmail.com>
2019-05-25 14:42:28 +0000

    add pset1 credit solution

--- pset1/credit.c
+++ pset1/credit.c
@@ -20,15 +20,46 @@ void luhn_algo(long number)
         length++;
     }
     // Iterate through digits from the end of the number
+    long divisor = 1;
+    long final_sum = 0;
+    long digit;
+    long prev_digit;
     for(int i = 0; i < length; i++)
     {    
-        long divisor = 1;
-        long digit = (number / divisor) % 10;
+        prev_digit = digit;
+        digit = (number / divisor) % 10;
         divisor *= 10;
+        // For every other digit starting from 2nd to last, multiply by two and add digits of product
         if(i % 2)    
         {    
             long product = digit * 2;
-            printf("%ld\n", product);
+            long product_first_digit = product / 10;
+            long product_second_digit = product % 10;
+            final_sum += product_first_digit;
+            final_sum += product_second_digit;
+        }
+        // For all remaining digits, add them to the total
+        else
+        {
+            final_sum += digit;
         }
     }
+    bool valid = (final_sum % 10 == 0);
+    if(valid && length == 15 && digit == 3 && (prev_digit == 4 || prev_digit == 7))
+    {
+        printf("AMEX\n");
+    }
+    if(valid && length == 16 && digit == 5 && (prev_digit == 1 || prev_digit == 2 || prev_digit ==3 || prev_digit == 4 || prev_digit == 5))
+    {
+        printf("MASTERCARD\n");
+    }
+    if(valid && (length == 13 || length == 16) && digit == 4)
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+    printf("FINAL SUM: %ld\n", final_sum);
 }
